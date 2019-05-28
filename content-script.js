0f32e30685696c924d625cf8b8f2995d504d13a0
arisehkawamura <harutaka.kawamura@arisenalytics.com>
2019-05-28 16:32:26 +0900

    done

--- content-script.js
+++ content-script.js
@@ -1,90 +1,32 @@
-const getHeaderLevel = h => {
-  return parseInt(h.slice(1));
+const hasOutput = cell => {
+  if (cell.querySelector('div.results.dashboard')) return true;
+
+  const output = cell.querySelector('div.ansiout');
+  if (!output) return false;
+
+  return output.textContent !== '';
 };
 
-const getMarkdownDiv = cell => {
-  return cell.querySelector('div.markdown');
+const toggleDisplay = element => {
+  const { display } = element.style;
+
+  if ((display === '') | (display == 'block')) {
+    element.style.display = 'none';
+  } else {
+    element.style.display = 'block';
+  }
 };
 
-const isMarkdownCell = cell => {
-  return getMarkdownDiv(cell) !== null;
-};
-
-const getHeaders = cell => {
-  return getMarkdownDiv(cell).querySelectorAll('h1, h2, h3, h4, h5, h6');
-};
-
-const hasHeader = cell => {
-  return getHeaders(cell).length > 0;
-};
-
-const getCellHref = cell => {
-  return cell.querySelector('a.command-number').getAttribute('href');
-};
-
-const parseHeader = mdCell => {
-  const href = getCellHref(mdCell);
-  const header = getHeaders(mdCell)[0];
-  const level = getHeaderLevel(header.tagName);
-  const text = header.textContent;
-  return {
-    level,
-    text,
-    href,
-  };
-};
-
-const makeListItem = ({ level, text, href }, topHeaderLevel) => {
-  const indent = '  '.repeat(level - topHeaderLevel);
-  return `${indent}- [${text}](${href})`;
-};
-
-const makeTOC = () => {
-  const cells = document.querySelectorAll('div.command-with-number');
-  const mdCells = [...cells].filter(c => isMarkdownCell(c) && hasHeader(c)).slice(1);
-  const headers = mdCells.map(parseHeader);
-  const topHeaderLevel = Math.min(...headers.map(({ level }) => level));
-  const TOC = headers.map(header => makeListItem(header, topHeaderLevel)).join('\n');
-  return TOC;
-};
-
-const getCellByHref = href => {
-  return document.querySelectorAll(`a[href='${href}'].command-number`)[0];
-};
-
-const mdCellExists = () => {
-  return document.querySelector('div.markdown') !== null;
-};
-
-const enableScrollToSection = () => {
-  if (!mdCellExists()) return;
-  const topMarkdownCell = document.querySelector('div.markdown');
-  const sectionLinks = topMarkdownCell.querySelectorAll("a[href^='#notebook']");
-  sectionLinks.forEach(sl => {
-    sl.style.fontWeight = 'bold';
-    sl.addEventListener('click', event => {
-      event.preventDefault();
-      const href = event.target.getAttribute('href');
-      const targetSection = getCellByHref(href);
-      targetSection.scrollIntoView();
-    });
-  });
-};
-
-const waitFor = (conditionFunc, func) => {
-  return () => {
-    const callback = () => {
-      if (conditionFunc()) {
-        clearInterval(handle);
-        func();
-      }
-    };
-    const handle = setInterval(callback, 100);
-  };
-};
-
-window.addEventListener('load', waitFor(mdCellExists, enableScrollToSection));
-document.addEventListener('mousedown', enableScrollToSection);
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  sendResponse(makeTOC());
+  const cells = document.querySelectorAll('div.command-with-number');
+
+  cells.forEach(cell => {
+    if (hasOutput(cell)) {
+      const code = cell.querySelector('div.command-input');
+      toggleDisplay(code);
+    } else {
+      toggleDisplay(cell);
+    }
+    sendResponse();
+  });
 });
--- popup.js
+++ popup.js
@@ -2,9 +2,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   const url = tabs[0].url;
   const pattern = /https:\/\/.*\.databricks.com\/#notebook\/.*/;
   if (url.match(pattern)) {
-    chrome.tabs.sendMessage(tabs[0].id, 'makeTOC', text => {
-      copyToClipboard(text);
-    });
+    chrome.tabs.sendMessage(tabs[0].id, 'makeTOC', () => {});
   } else {
     const message = document.getElementById('message');
     message.textContent = 'This page is not a Databricks notebook';
