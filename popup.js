bf27f26f958f47d5e07934a5ce0c98fd9208acaa
harupy <hkawamura0130@gmail.com>
2019-05-29 10:57:17 +0900

    remove unused function

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   const url = tabs[0].url;
-  const pattern = /https:\/\/.*\.databricks.com\/#notebook\/.*/;
+  const pattern = /https:\/\/.*\.databricks.com\/.*#notebook\/.*/;
   if (url.match(pattern)) {
     chrome.tabs.sendMessage(tabs[0].id, 'makeTOC', () => {});
   } else {
@@ -9,13 +9,3 @@ chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     message.style.color = 'red';
   }
 });
-
-const copyToClipboard = text => {
-  const copyFrom = document.createElement('textarea');
-  copyFrom.textContent = text;
-  document.body.appendChild(copyFrom);
-  copyFrom.select();
-  document.execCommand('copy');
-  copyFrom.blur();
-  document.body.removeChild(copyFrom);
-};
